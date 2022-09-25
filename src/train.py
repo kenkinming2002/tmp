6634442f406a04daaaab04ec7a3052537444fd4c
KenKwok <kenkinming2002@gmail.com>
2022-09-25 16:17:52 +0800

    Better normalization(hopefully)

--- src/train.py
+++ src/train.py
@@ -136,12 +136,37 @@ print(f"Before normalization - Input:  mean = {np.mean(all_input,  axis=0)}, std
 print(f"Before normalization - Output: mean = {np.mean(all_output, axis=0)}, std = {np.std(all_output, axis=0)}")
 
 # Normalize
-all_input  = (all_input  - np.mean(all_input,  axis=0)) / np.std(all_input,  axis=0)
+#all_input  = (all_input  - np.mean(all_input,  axis=0)) / np.std(all_input,  axis=0)
 #all_input  = (all_input  - np.amin(all_input,  axis=0)) / (np.amax(all_input,  axis=0) - np.amin(all_input,  axis=0)) * 2.0 - 1.0
+#all_input = (all_input - np.median(all_input, axis=0)) / np.std(all_input, axis=0)
+all_input = (all_input - np.median(all_input, axis=0)) / (np.amax(all_input,  axis=0) - np.amin(all_input,  axis=0))
 
-all_output = (all_output - np.mean(all_output, axis=0)) / np.std(all_output, axis=0)
+#all_output = (all_output - np.mean(all_output, axis=0)) / np.std(all_output, axis=0)
 #all_output = (all_output - np.amin(all_output, axis=0)) / (np.amax(all_output, axis=0) - np.amin(all_output, axis=0)) * 2.0 - 1.0
 #all_output = (all_output - np.median(all_output, axis=0)) / np.std(all_output, axis=0)
+all_output = (all_output - np.median(all_output, axis=0)) / (np.amax(all_output, axis=0) - np.amin(all_output, axis=0))
+
+fig, axs = plt.subplots(1, 7)
+
+axs[0].hist(all_input[...,0], bins = 100)
+axs[1].hist(all_input[...,1], bins = 100)
+axs[2].hist(all_input[...,2], bins = 100)
+axs[3].hist(all_input[...,3], bins = 100)
+axs[4].hist(all_input[...,4], bins = 100)
+axs[5].hist(all_input[...,5], bins = 100)
+
+axs[6].hist(all_output[...,0], bins = 100)
+
+axs[0].set_title("AOD")
+axs[1].set_title("PBLH")
+axs[2].set_title("PS")
+axs[3].set_title("QV10M")
+axs[4].set_title("T2M")
+axs[5].set_title("WS")
+
+axs[6].set_title("PM25")
+
+plt.show()
 
 # Debug
 print(f"After normalization - Input:  mean = {np.mean(all_input,  axis=0)}, std = {np.std(all_input,  axis=0)}")
