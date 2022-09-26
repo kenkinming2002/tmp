65062e0746a882f42e08535e46bd84335b16dc1f
KenKwok <kenkinming2002@gmail.com>
2022-09-26 22:33:43 +0800

    Add train_base.py

--- src/train.py
+++ src/train.py
@@ -2,11 +2,10 @@
 
 from tqdm import tqdm
 
+from train_base import *
+
 import sys
 import math
-
-import numpy as np
-import matplotlib.pyplot as plt
 
 class DenseLayer:
     def __init__(self, input_size, output_size):
@@ -114,74 +113,6 @@ def test(model, testing_input, testing_output):
     ss_tot = np.square(real_testing_output - real_testing_output.mean()).sum()
     r2 = 1 - ss_res / ss_tot
     print(f"Testing:ss_res={ss_res}, ss_tot={ss_tot}, R2={r2}")
-
-# Load
-with open('dataset', 'rb') as f:
-    all_input  = np.load(f, allow_pickle = True)
-    all_output = np.load(f, allow_pickle = True)
-
-tmp = np.random.randint(0, all_input.shape[0], 100)
-all_input  = all_input[tmp]
-all_output = all_output[tmp]
-
-all_input_mean = np.mean(all_input, axis=0)
-all_input_std  = np.std(all_input, axis=0)
-
-all_output_mean = np.mean(all_output, axis=0)
-all_output_std  = np.std (all_output, axis=0)
-
-#all_input  = np.random.uniform(-0.3, 0.3, (50000, 6))
-#all_output = np.sum(all_input, 1, keepdims = True)
-
-count = all_output.shape[0]
-
-print(all_input.shape)
-print(all_output.shape)
-
-print(np.count_nonzero(np.isnan(all_input)))
-print(np.count_nonzero(np.isnan(all_output)))
-
-# Debug
-print(f"Before normalization - Input:  mean = {np.mean(all_input,  axis=0)}, std = {np.std(all_input,  axis=0)}")
-print(f"Before normalization - Output: mean = {np.mean(all_output, axis=0)}, std = {np.std(all_output, axis=0)}")
-
-fig, axs = plt.subplots(1, 7)
-
-axs[0].hist(all_input[...,0], bins = 100)
-axs[1].hist(all_input[...,1], bins = 100)
-axs[2].hist(all_input[...,2], bins = 100)
-axs[3].hist(all_input[...,3], bins = 100)
-axs[4].hist(all_input[...,4], bins = 100)
-axs[5].hist(all_input[...,5], bins = 100)
-
-axs[6].hist(all_output[...,0], bins = 100)
-
-axs[0].set_title("AOD")
-axs[1].set_title("PBLH")
-axs[2].set_title("PS")
-axs[3].set_title("QV10M")
-axs[4].set_title("T2M")
-axs[5].set_title("WS")
-
-axs[6].set_title("PM25")
-
-plt.show()
-fig.savefig('fig.png')
-
-# Normalize
-all_input  = (all_input  - all_input_mean) / all_input_std
-#all_input  = (all_input  - np.amin(all_input,  axis=0)) / (np.amax(all_input,  axis=0) - np.amin(all_input,  axis=0)) * 2.0 - 1.0
-#all_input = (all_input - np.median(all_input, axis=0)) / np.std(all_input, axis=0)
-#all_input = (all_input - np.median(all_input, axis=0)) / (np.amax(all_input,  axis=0) - np.amin(all_input,  axis=0))
-
-all_output = (all_output - all_output_mean) / all_output_std
-#all_output = (all_output - np.amin(all_output, axis=0)) / (np.amax(all_output, axis=0) - np.amin(all_output, axis=0)) * 2.0 - 1.0
-#all_output = (all_output - np.median(all_output, axis=0)) / np.std(all_output, axis=0)
-#all_output = (all_output - np.median(all_output, axis=0)) / (np.amax(all_output, axis=0) - np.amin(all_output, axis=0))
-
-# Debug
-print(f"After normalization - Input:  mean = {np.mean(all_input,  axis=0)}, std = {np.std(all_input,  axis=0)}")
-print(f"After normalization - Output: mean = {np.mean(all_output, axis=0)}, std = {np.std(all_output, axis=0)}")
 
 sub_count = int(count / 10)
 for i in range(10):
--- src/train_torch.py
+++ src/train_torch.py
@@ -2,6 +2,8 @@
 
 from tqdm import tqdm
 
+from train_base import *
+
 import sys
 import math
 
@@ -10,82 +12,14 @@ from torch import nn
 from torch.utils.data import TensorDataset, DataLoader
 
 import numpy as np
-import matplotlib.pyplot as plt
-
-
-#################################### Load and prepare the data set #############################################################
-
-# Load
-with open('dataset', 'rb') as f:
-    all_input  = np.load(f, allow_pickle = True)
-    all_output = np.load(f, allow_pickle = True)
-
-#all_input  = np.random.uniform(-0.3, 0.3, (50000, 6))
-#all_output = np.sum(all_input, 1, keepdims = True)
-
-count = all_output.shape[0]
-
-print(all_input.shape)
-print(all_output.shape)
-
-print(np.count_nonzero(np.isnan(all_input)))
-print(np.count_nonzero(np.isnan(all_output)))
-
-# Shuffle
-tmp = np.random.permutation(count)
-all_input  = all_input[tmp]
-all_output = all_output[tmp]
-
-# Debug
-print(f"Before normalization - Input:  mean = {np.mean(all_input,  axis=0)}, std = {np.std(all_input,  axis=0)}")
-print(f"Before normalization - Output: mean = {np.mean(all_output, axis=0)}, std = {np.std(all_output, axis=0)}")
-
-# Normalize
-#all_input  = (all_input  - np.mean(all_input,  axis=0)) / np.std(all_input,  axis=0)
-#all_input  = (all_input  - np.amin(all_input,  axis=0)) / (np.amax(all_input,  axis=0) - np.amin(all_input,  axis=0)) * 2.0 - 1.0
-#all_input = (all_input - np.median(all_input, axis=0)) / np.std(all_input, axis=0)
-all_input = (all_input - np.median(all_input, axis=0)) / (np.amax(all_input,  axis=0) - np.amin(all_input,  axis=0))
-
-#all_output = (all_output - np.mean(all_output, axis=0)) / np.std(all_output, axis=0)
-#all_output = (all_output - np.amin(all_output, axis=0)) / (np.amax(all_output, axis=0) - np.amin(all_output, axis=0)) * 2.0 - 1.0
-#all_output = (all_output - np.median(all_output, axis=0)) / np.std(all_output, axis=0)
-all_output = (all_output - np.median(all_output, axis=0)) / (np.amax(all_output, axis=0) - np.amin(all_output, axis=0))
-
-fig, axs = plt.subplots(1, 7)
-
-axs[0].hist(all_input[...,0], bins = 100)
-axs[1].hist(all_input[...,1], bins = 100)
-axs[2].hist(all_input[...,2], bins = 100)
-axs[3].hist(all_input[...,3], bins = 100)
-axs[4].hist(all_input[...,4], bins = 100)
-axs[5].hist(all_input[...,5], bins = 100)
-
-axs[6].hist(all_output[...,0], bins = 100)
-
-axs[0].set_title("AOD")
-axs[1].set_title("PBLH")
-axs[2].set_title("PS")
-axs[3].set_title("QV10M")
-axs[4].set_title("T2M")
-axs[5].set_title("WS")
-
-axs[6].set_title("PM25")
-
-plt.show()
-
-# Debug
-print(f"After normalization - Input:  mean = {np.mean(all_input,  axis=0)}, std = {np.std(all_input,  axis=0)}")
-print(f"After normalization - Output: mean = {np.mean(all_output, axis=0)}, std = {np.std(all_output, axis=0)}")
-
-#################################### Optimization #############################################################
 
 class Model(nn.Module):
     def __init__(self):
         super(Model, self).__init__()
         self.stack = nn.Sequential(
-            nn.Linear(6, 16),
+            nn.Linear(6, 32),
             nn.Tanh(),
-            nn.Linear(16, 8),
+            nn.Linear(32, 8),
             nn.Tanh(),
             nn.Linear(8, 4),
             nn.Tanh(),
@@ -173,7 +107,7 @@ for i in range(10):
     print("Testing on training data")
     test(model, 128, training_dataset)
 
-    for i in range(100):
+    while True:
         train(model, 1, 32, 0.05, training_dataset)
 
         print("Testing on testing data")
