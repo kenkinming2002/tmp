abff7e1a1145232adca2c8f869490ca190c32a37
KenKwok <kenkinming2002@gmail.com>
2022-10-01 18:13:15 +0800

    Reimplement MERRA processing

--- src/main.py
+++ src/main.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python
 
-from aod import *
+from aod   import *
+from merra import *
 
 aod_data = load_aod()
 for i in range(10):
     print(aod_data[i])
+
+merra_data = load_merra("data/Meteorological_Data/PBLH", "PBLH")
+for i in range(24 * 59):
+    if merra_data[i] is None:
+        print("None")
+    else:
+        print(merra_data[i].shape)
---
+++ src/merra.py
@@ -0,0 +1,33 @@
+from tqdm import tqdm
+
+from netCDF4 import Dataset
+import numpy as np
+import os
+
+# Return list of scatter data indexed by time
+# Grid data indexed by [lat][lon]
+def get_data_nc(filename, name):
+    f = Dataset(filename)
+    data = f[name][:]
+    return data
+
+def load_merra(dirname, name):
+    def filename_as_date(filename):
+        month = int(filename[31:33])
+        day   = int(filename[33:35])
+        if month == 1:
+            return day - 1
+        elif month == 2:
+            return day + 31 - 1
+        else:
+            raise Exception(f"Invalid month={month}")
+
+    res = [None] * 24 * 59
+    for filename in tqdm(os.listdir(dirname), desc=f"MERRA(dirname={dirname},name={name})"):
+        date = filename_as_date(filename)
+        data = get_data_nc(f"{dirname}/{filename}", name)
+        for hour in range(11):
+            time = date * 24 + hour
+            res[time] = data[hour]
+
+    return res
