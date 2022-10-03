76f2def0822043a5b30a2a2d24250e1646e432a0
KenKwok <kenkinming2002@gmail.com>
2022-10-03 15:59:46 +0800

    Add sklearn implementation

---
+++ src/train_sklearn.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from sklearn import preprocessing
+from sklearn.neural_network import MLPRegressor
+from sklearn.model_selection import train_test_split
+
+import numpy as np
+
+X = np.load("stage4/input.npy")
+Y = np.load("stage4/output.npy")
+
+X_scaler = preprocessing.StandardScaler().fit(X)
+Y_scaler = preprocessing.StandardScaler().fit(Y)
+
+X_scaled = X_scaler.transform(X)
+Y_scaled = Y_scaler.transform(Y)
+
+X_train, X_test, y_train, y_test = train_test_split(X_scaled, Y_scaled, random_state=1)
+
+regr = MLPRegressor(hidden_layer_sizes=(128, 64, 32), activation='tanh', random_state=1).fit(X_train, y_train)
+print(regr.predict(X_test[:2]))
+print(regr.score(X_test, y_test))
+
+
