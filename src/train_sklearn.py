d509c0be5da92591ae40e086263ee251382049e2
KenKwok <kenkinming2002@gmail.com>
2022-10-03 18:40:48 +0800

    Add kfold cross validation

--- src/train_sklearn.py
+++ src/train_sklearn.py
@@ -3,6 +3,7 @@
 from sklearn import preprocessing
 from sklearn.neural_network import MLPRegressor
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
 
 import numpy as np
 
@@ -15,11 +16,15 @@ Y_scaler = preprocessing.StandardScaler().fit(Y)
 X_scaled = X_scaler.transform(X)
 Y_scaled = Y_scaler.transform(Y)
 
-X_train, X_test, y_train, y_test = train_test_split(X_scaled, Y_scaled, random_state=1)
+kf = KFold(n_splits=10, random_state=None,shuffle=False)
+for train_index, test_index in kf.split(X_scaled):
+    X_train, X_test = X_scaled[train_index], X_scaled[test_index]
+    Y_train, Y_test = Y_scaled[train_index], Y_scaled[test_index]
+    regr = MLPRegressor(hidden_layer_sizes=(16, 8, 4), activation='tanh', solver='sgd', learning_rate='adaptive', max_iter=1000, random_state=1).fit(X_train, Y_train)
 
-regr = MLPRegressor(hidden_layer_sizes=(32, 32, 32), activation='tanh', solver='sgd', learning_rate = 'adaptive', max_iter=1000, random_state=1).fit(X_train, y_train)
-print(regr.predict(X_test[:2]))
-print(regr.score(X_train, y_train))
-print(regr.score(X_test, y_test))
+    print(regr.predict(X_test[:2]))
+    print(Y_test[:2])
+    print(regr.score(X_train, Y_train))
+    print(regr.score(X_test, Y_test))
 
 
