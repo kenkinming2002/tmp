b9a8cbda105e4313bd9c930a80ff182f1985bb17
KenKwok <kenkinming2002@gmail.com>
2022-10-08 23:15:18 +0800

    Add searching and plotting

--- src/train_sklearn.py
+++ src/train_sklearn.py
@@ -5,7 +5,12 @@ from sklearn.neural_network import MLPRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import KFold
 
+from tqdm import tqdm
+
 import numpy as np
+import matplotlib.pyplot as plt
+
+import os
 
 X = np.load("stage4/input.npy")
 Y = np.load("stage4/output.npy")
@@ -17,14 +22,36 @@ X_scaled = X_scaler.transform(X)
 Y_scaled = Y_scaler.transform(Y)
 
 kf = KFold(n_splits=10, random_state=None,shuffle=False)
-for train_index, test_index in kf.split(X_scaled):
+for i, (train_index, test_index) in tqdm(enumerate(kf.split(X_scaled)), desc="index", position=0, leave=False):
     X_train, X_test = X_scaled[train_index], X_scaled[test_index]
     Y_train, Y_test = Y_scaled[train_index], Y_scaled[test_index]
-    regr = MLPRegressor(hidden_layer_sizes=(16, 8, 4), activation='tanh', solver='sgd', learning_rate='adaptive', max_iter=1000, random_state=1).fit(X_train, Y_train)
+    for hidden_layer_sizes in tqdm([(16, 8, 4), (64, 64, 64)], desc="hidden_layer_sizes", position=1, leave=False):
+        for learning_rate in tqdm([0.05, 0.005], desc="learning_rate", position=2, leave=False):
+            regr = MLPRegressor(hidden_layer_sizes=hidden_layer_sizes, learning_rate_init=learning_rate, solver='sgd', max_iter=1000, random_state=1)
+            regr.fit(X_train, Y_train.ravel())
 
-    print(regr.predict(X_test[:2]))
-    print(Y_test[:2])
-    print(regr.score(X_train, Y_train))
-    print(regr.score(X_test, Y_test))
+            dirname = f"results/{i}/{'_'.join([str(x) for x in hidden_layer_sizes])}/{learning_rate}"
+            os.makedirs(dirname, exist_ok = True)
 
+            if True:
+                fig, ax = plt.subplots(1,1)
+                ax.plot(np.linspace(0, np.size(regr.loss_curve_), np.size(regr.loss_curve_)), regr.loss_curve_)
+                fig.savefig(f"{dirname}/loss_curve.png")
+                plt.close(fig)
+
+            if True:
+                fig, ax = plt.subplots(1,1)
+                scatter = ax.scatter(Y_scaler.inverse_transform(regr.predict(X_train).reshape(-1,1)), Y_scaler.inverse_transform(Y_train))
+                scatter.set_label(f"$R^2=${regr.score(X_train, Y_train)}")
+                ax.legend()
+                fig.savefig(f"{dirname}/train.png")
+                plt.close(fig)
+
+            if True:
+                fig, ax = plt.subplots(1,1)
+                scatter = ax.scatter(Y_scaler.inverse_transform(regr.predict(X_test).reshape(-1,1)), Y_scaler.inverse_transform(Y_test))
+                scatter.set_label(f"$R^2=${regr.score(X_test, Y_test)}")
+                ax.legend()
+                fig.savefig(f"{dirname}/test.png")
+                plt.close(fig)
 
